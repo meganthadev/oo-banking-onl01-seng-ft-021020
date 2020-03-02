938350e98df034b31df46d8eae97d9b1d1aa1e2a
Megan Horsfield <meganthadev@gmail.com>
2020-03-02 05:28:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,12 @@
 class Transfer
-  # your code here
+
+  attr_reader :sender
+  attr_accessor :receiver, :status
+
+  def initialize(name)
+    @name = name
+    @receiver = 1000
+    @status = "pending"
+  end  
+  
 end
